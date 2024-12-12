9dc96a75d0908250333832c9da47f48ec3ad804b
Orçun Aşık <asikorcun@gmail.com>
2024-12-12 21:50:45 +0300

    Added DrinkController and List ViewResult method

---
+++ CoreDrink.WebUI/Controllers/DrinkController.cs
@@ -0,0 +1,21 @@
+﻿using CoreDrink.WebUI.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDrink.WebUI.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IDrinkRepository _drinkRepository;
+        public DrinkController(ICategoryRepository categoryRepository, IDrinkRepository drinkRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _drinkRepository = drinkRepository;
+        }
+        public ViewResult List()
+        {
+            var drinks = _drinkRepository.Drinks;
+            return View(drinks);
+        }
+    }
+}
