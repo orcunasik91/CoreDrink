9a4f92c11434cdbdeffe8fe4c6459c7046ef8380
Orçun Aşık <asikorcun@gmail.com>
2024-12-12 22:01:45 +0300

    Added DrinkListViewModel

---
+++ CoreDrink.WebUI/ViewModels/DrinkListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CoreDrink.WebUI.Data.Models;
+using System.Collections.Generic;
+
+namespace CoreDrink.WebUI.ViewModels
+{
+    public class DrinkListViewModel
+    {
+        public IEnumerable<Drink> Drinks { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
