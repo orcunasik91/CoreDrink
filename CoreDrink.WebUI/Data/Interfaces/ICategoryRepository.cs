fb0eeef2394b41d2fd3c19d59de7da064832eac6
Orçun Aşık <asikorcun@gmail.com>
2024-12-12 21:24:49 +0300

    Added interfaces for category and drink models.

---
+++ CoreDrink.WebUI/Data/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using CoreDrink.WebUI.Data.Models;
+using System.Collections.Generic;
+
+namespace CoreDrink.WebUI.Data.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> Categories { get; }
+    }
+}
---
+++ CoreDrink.WebUI/Data/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,12 @@
+﻿using CoreDrink.WebUI.Data.Models;
+using System.Collections.Generic;
+
+namespace CoreDrink.WebUI.Data.Interfaces
+{
+    public interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; set; }
+        IEnumerable<Drink> PreferredDrinks { get; set; }
+        Drink GetDrinkById(int drinkId);
+    }
+}
