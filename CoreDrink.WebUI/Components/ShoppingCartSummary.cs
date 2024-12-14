2e0b7a0443788d6ed0af71d7056955c48058ad21
Orçun Aşık <asikorcun@gmail.com>
2024-12-14 15:57:52 +0300

    Created ShoppingCartSummary named ViewComponent and View page.

---
+++ CoreDrink.WebUI/Components/ShoppingCartSummary.cs
@@ -0,0 +1,32 @@
+﻿using CoreDrink.WebUI.Data.Models;
+using CoreDrink.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDrink.WebUI.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            //var items = _shoppingCart.GetShoppingCartItems();
+            var items = new List<ShoppingCartItem>()
+            {
+                new ShoppingCartItem(),
+                new ShoppingCartItem()
+            };
+            _shoppingCart.ShoppingCartItems = items;
+            ShoppingCartViewModel shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
