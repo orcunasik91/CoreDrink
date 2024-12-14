8a3b3fdd0e02919276a54a85ccc19d581848b0a3
Orçun Aşık <asikorcun@gmail.com>
2024-12-14 14:53:01 +0300

    Added HomeController and Index view

---
+++ CoreDrink.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using CoreDrink.WebUI.Data.Interfaces;
+using CoreDrink.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDrink.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+        public HomeController(IDrinkRepository drinkRepository)
+        {
+            _drinkRepository = drinkRepository;
+        }
+        public ViewResult Index()
+        {
+            HomeViewModel homeViewModel = new HomeViewModel
+            {
+                PreferredDrinks = _drinkRepository.PreferredDrinks
+            };
+            return View(homeViewModel);
+        }
+    }
+}
