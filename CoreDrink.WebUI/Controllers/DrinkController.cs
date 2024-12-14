863d1c1969b201fc43794de1a0050c2520fc83bc
Orçun Aşık <asikorcun@gmail.com>
2024-12-14 23:41:56 +0300

    List method was edited for dynamic category menu

--- CoreDrink.WebUI/Controllers/DrinkController.cs
+++ CoreDrink.WebUI/Controllers/DrinkController.cs
@@ -1,6 +1,10 @@
 ﻿using CoreDrink.WebUI.Data.Interfaces;
+using CoreDrink.WebUI.Data.Models;
 using CoreDrink.WebUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreDrink.WebUI.Controllers
 {
@@ -13,12 +17,34 @@ namespace CoreDrink.WebUI.Controllers
             _categoryRepository = categoryRepository;
             _drinkRepository = drinkRepository;
         }
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            DrinkListViewModel vm = new DrinkListViewModel();
-            vm.Drinks = _drinkRepository.Drinks;
-            vm.CurrentCategory = "İçecek Kategorisi";
-            return View(vm);
+            string _category = category;
+            IEnumerable<Drink> drinks;
+            string currentCategory = string.Empty;
+            if (string.IsNullOrEmpty(category))
+            {
+                drinks = _drinkRepository.Drinks.OrderBy(n => n.DrinkId);
+                currentCategory = "Tüm içecekler";
+            }
+            else
+            {
+                if (string.Equals("Alkollü", _category,StringComparison.OrdinalIgnoreCase))
+                {
+                    drinks = _drinkRepository.Drinks.Where(p => p.Category.CategoryName.Equals("Alkollü")).OrderBy(n => n.DrinkId);
+                }
+                else
+                {
+                    drinks = _drinkRepository.Drinks.Where(p => p.Category.CategoryName.Equals("Alkolsüz")).OrderBy(n => n.DrinkId);
+                }
+                currentCategory = category;
+            }
+            DrinkListViewModel drinkListViewModel = new DrinkListViewModel
+            {
+                Drinks = drinks,
+                CurrentCategory = currentCategory
+            };
+            return View(drinkListViewModel);
         }
     }
 }
