762210ffba2155fe6e48db820bb30e12173fbdee
Orçun Aşık <asikorcun@gmail.com>
2024-12-14 14:51:46 +0300

    Added ShoppingCartViewModel for user

---
+++ CoreDrink.WebUI/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CoreDrink.WebUI.Data.Models;
+
+namespace CoreDrink.WebUI.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
