d410ce9074637675d6635022c9af7abf352db950
Orçun Aşık <asikorcun@gmail.com>
2024-12-14 14:52:08 +0300

    Added HomeViewModel for user

---
+++ CoreDrink.WebUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CoreDrink.WebUI.Data.Models;
+using System.Collections.Generic;
+
+namespace CoreDrink.WebUI.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Drink> PreferredDrinks { get; set; }
+    }
+}
