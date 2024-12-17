043d21bb60da350409a457ffa5626c7a0c4987d4
Orçun Aşık <asikorcun@gmail.com>
2024-12-17 14:47:28 +0300

    Added migration for order and updated database

--- CoreDrink.WebUI/Migrations/20241217114208_OrdersandOrderDetails.Designer.cs
+++ CoreDrink.WebUI/Migrations/20241217114208_OrdersandOrderDetails.Designer.cs
@@ -4,14 +4,16 @@ using CoreDrink.WebUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreDrink.WebUI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241217114208_OrdersandOrderDetails")]
+    partial class OrdersandOrderDetails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
