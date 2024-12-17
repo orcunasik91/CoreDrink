f741d604f525aaf739645dfe77326dc074093180
Orçun Aşık <asikorcun@gmail.com>
2024-12-17 16:23:59 +0300

    Added "City" column for Order model and updated database.

--- CoreDrink.WebUI/Migrations/20241217125624_CityColumnForOrder.Designer.cs
+++ CoreDrink.WebUI/Migrations/20241217125624_CityColumnForOrder.Designer.cs
@@ -4,14 +4,16 @@ using CoreDrink.WebUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreDrink.WebUI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241217125624_CityColumnForOrder")]
+    partial class CityColumnForOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
