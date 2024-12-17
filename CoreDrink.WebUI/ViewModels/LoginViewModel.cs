10071be99a38c22e955b257b35ea00af07dc5b7c
Orçun Aşık <asikorcun@gmail.com>
2024-12-17 20:19:37 +0300

    Added LoginViewModel for Login and Register operations

---
+++ CoreDrink.WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreDrink.WebUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Kullanıcı Adı")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
