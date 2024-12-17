2cb665c8b8349cf26d2f83d0bcf73e2e3d9e298f
Orçun Aşık <asikorcun@gmail.com>
2024-12-17 16:29:22 +0300

    Created OrderController and Views

---
+++ CoreDrink.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using CoreDrink.WebUI.Data.Interfaces;
+using CoreDrink.WebUI.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDrink.WebUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Sepetiniz boş, birkaç ürün ekleyin!");
+            }
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Siparişiniz İçin Teşekkürler :)";
+            return View();
+        }
+    }
+}
