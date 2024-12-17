ab55b65332f120e615c9a20342304ee1de4b83ed
Orçun Aşık <asikorcun@gmail.com>
2024-12-17 20:18:57 +0300

    Created AccountController, Login,register and Logout view

---
+++ CoreDrink.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using CoreDrink.WebUI.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreDrink.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+            IdentityUser user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false,false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+                        return RedirectToAction("Index", "Home");
+                    return Redirect(loginViewModel.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Kullanıcı/Parola Geçerli Değil");
+            return View(loginViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async  Task<IActionResult> Register(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser() { UserName = loginViewModel.UserName };
+                IdentityResult result = await _userManager.CreateAsync(user, loginViewModel.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
